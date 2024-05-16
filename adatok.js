9a5ac79e1c4e65169bf697a5fb3fd3c76093fb2c
barossboti <150018760+barossboti@users.noreply.github.com>
2024-05-16 10:49:26 +0200

    Add files via upload

---
+++ adatok.js
@@ -0,0 +1,341 @@
+var  hírességek =
+[
+    {
+      "name": "Beyoncé",
+      "occupation": "Singer",
+      "age": 40,
+      "birthplace": "Houston, Texas"
+    },
+    {
+      "name": "Tom Hanks",
+      "occupation": "Actor",
+      "age": 65,
+      "birthplace": "Concord, California"
+    },
+    {
+      "name": "Oprah Winfrey",
+      "occupation": "TV Host",
+      "age": 68,
+      "birthplace": "Kosciusko, Mississippi"
+    },
+    {
+      "name": "LeBron James",
+      "occupation": "Basketball Player",
+      "age": 38,
+      "birthplace": "Akron, Ohio"
+    },
+    {
+      "name": "Taylor Swift",
+      "occupation": "Singer",
+      "age": 34,
+      "birthplace": "Reading, Pennsylvania"
+    },
+    {
+      "name": "Denzel Washington",
+      "occupation": "Actor",
+      "age": 67,
+      "birthplace": "Mount Vernon, New York"
+    },
+    {
+      "name": "Ellen DeGeneres",
+      "occupation": "TV Host",
+      "age": 64,
+      "birthplace": "Metairie, Louisiana"
+    },
+    {
+      "name": "Michael Jordan",
+      "occupation": "Basketball Player",
+      "age": 59,
+      "birthplace": "Brooklyn, New York"
+    },
+    {
+      "name": "Angelina Jolie",
+      "occupation": "Actor",
+      "age": 46,
+      "birthplace": "Los Angeles, California"
+    },
+    {
+      "name": "Bill Gates",
+      "occupation": "Entrepreneur",
+      "age": 68,
+      "birthplace": "Seattle, Washington"
+    },
+    {
+      "name": "Jennifer Lawrence",
+      "occupation": "Actor",
+      "age": 33,
+      "birthplace": "Indian Hills, Kentucky"
+    },
+    {
+      "name": "Ellen Pompeo",
+      "occupation": "Actor",
+      "age": 52,
+      "birthplace": "Everett, Massachusetts"
+    },
+    {
+      "name": "Leonardo DiCaprio",
+      "occupation": "Actor",
+      "age": 48,
+      "birthplace": "Los Angeles, California"
+    },
+    {
+      "name": "Serena Williams",
+      "occupation": "Tennis Player",
+      "age": 40,
+      "birthplace": "Saginaw, Michigan"
+    },
+    {
+      "name": "Mark Zuckerberg",
+      "occupation": "Entrepreneur",
+      "age": 39,
+      "birthplace": "White Plains, New York"
+    },
+    {
+      "name": "Johnny Depp",
+      "occupation": "Actor",
+      "age": 59,
+      "birthplace": "Owensboro, Kentucky"
+    },
+    {
+      "name": "Stephen Curry",
+      "occupation": "Basketball Player",
+      "age": 34,
+      "birthplace": "Akron, Ohio"
+    },
+    {
+      "name": "Meryl Streep",
+      "occupation": "Actor",
+      "age": 73,
+      "birthplace": "Summit, New Jersey"
+    },
+    {
+      "name": "Elon Musk",
+      "occupation": "Entrepreneur",
+      "age": 52,
+      "birthplace": "Pretoria, South Africa"
+    },
+    {
+      "name": "Jennifer Aniston",
+      "occupation": "Actor",
+      "age": 54,
+      "birthplace": "Sherman Oaks, California"
+    },
+    {
+      "name": "Will Smith",
+      "occupation": "Actor",
+      "age": 54,
+      "birthplace": "Philadelphia, Pennsylvania"
+    },
+    {
+      "name": "Scarlett Johansson",
+      "occupation": "Actor",
+      "age": 38,
+      "birthplace": "New York City, New York"
+    },
+    {
+      "name": "Kobe Bryant",
+      "occupation": "Basketball Player",
+      "age": 41,
+      "birthplace": "Philadelphia, Pennsylvania"
+    },
+    {
+      "name": "Emma Watson",
+      "occupation": "Actor",
+      "age": 32,
+      "birthplace": "Paris, France"
+    },
+    {
+      "name": "David Beckham",
+      "occupation": "Soccer Player",
+      "age": 47,
+      "birthplace": "London, England"
+    },
+    {
+      "name": "Adele",
+      "occupation": "Singer",
+      "age": 34,
+      "birthplace": "London, England"
+    },
+    {
+      "name": "George Clooney",
+      "occupation": "Actor",
+      "age": 61,
+      "birthplace": "Lexington, Kentucky"
+    },
+    {
+      "name": "Rihanna",
+      "occupation": "Singer",
+      "age": 35,
+      "birthplace": "Saint Michael, Barbados"
+    },
+    {
+      "name": "Michelle Obama",
+      "occupation": "Former First Lady",
+      "age": 59,
+      "birthplace": "Chicago, Illinois"
+    },
+    {
+      "name": "Matthew McConaughey",
+      "occupation": "Actor",
+      "age": 53,
+      "birthplace": "Uvalde, Texas"
+    },
+    {
+      "name": "Brad Pitt",
+      "occupation": "Actor",
+      "age": 60,
+      "birthplace": "Shawnee, Oklahoma"
+    },
+    {
+      "name": "Eminem",
+      "occupation": "Rapper",
+      "age": 50,
+      "birthplace": "St. Joseph, Missouri"
+    },
+    {
+      "name": "Kanye West",
+      "occupation": "Rapper",
+      "age": 46,
+      "birthplace": "Atlanta, Georgia"
+    },
+    {
+      "name": "Justin Bieber",
+      "occupation": "Singer",
+      "age": 30,
+      "birthplace": "London, Canada"
+    },
+    {
+      "name": "Lady Gaga",
+      "occupation": "Singer",
+      "age": 38,
+      "birthplace": "New York City, New York"
+    },
+    {
+      "name": "Chris Pratt",
+      "occupation": "Actor",
+      "age": 44,
+      "birthplace": "Virginia, Minnesota"
+    },
+    {
+      "name": "Tom Brady",
+      "occupation": "Football Player",
+      "age": 46,
+      "birthplace": "San Mateo, California"
+    },
+    {
+      "name": "Selena Gomez",
+      "occupation": "Singer",
+      "age": 30,
+      "birthplace": "Grand Prairie, Texas"
+    },
+    {
+      "name": "Robert Downey Jr.",
+      "occupation": "Actor",
+      "age": 58,
+      "birthplace": "New York City, New York"
+    },
+    {
+      "name": "Emma Stone",
+      "occupation": "Actor",
+      "age": 35,
+      "birthplace": "Scottsdale, Arizona"
+    },
+    {
+      "name": "Nicki Minaj",
+      "occupation": "Rapper",
+      "age": 40,
+      "birthplace": "Port of Spain, Trinidad and Tobago"
+    },
+    {
+      "name": "Drake",
+      "occupation": "Rapper",
+      "age": 36,
+      "birthplace": "Toronto, Canada"
+    },
+    {
+      "name": "Kristen Stewart",
+      "occupation": "Actor",
+      "age": 34,
+      "birthplace": "Los Angeles, California"
+    },
+    {
+      "name": "Chris Hemsworth",
+      "occupation": "Actor",
+      "age": 39,
+      "birthplace": "Melbourne, Australia"
+    },
+    {
+      "name": "Anne Hathaway",
+      "occupation": "Actor",
+      "age": 40,
+      "birthplace": "Brooklyn, New York"
+    },
+    {
+      "name": "Natalie Portman",
+      "occupation": "Actor",
+      "age": 41,
+      "birthplace": "Jerusalem, Israel"
+    },
+    {
+      "name": "Katy Perry",
+      "occupation": "Singer",
+      "age": 38,
+      "birthplace": "Santa Barbara, California"
+    },
+    {
+      "name": "Daniel Radcliffe",
+      "occupation": "Actor",
+      "age": 33,
+      "birthplace": "London, England"
+    },
+    {
+      "name": "Julia Roberts",
+      "occupation": "Actor",
+      "age": 56,
+      "birthplace": "Smyrna, Georgia"
+    },
+    {
+      "name": "Snoop Dogg",
+      "occupation": "Rapper",
+      "age": 51,
+      "birthplace": "Long Beach, California"
+    },
+    {
+      "name": "Miley Cyrus",
+      "occupation": "Singer",
+      "age": 31,
+      "birthplace": "Franklin, Tennessee"
+    },
+    {
+      "name": "Hugh Jackman",
+      "occupation": "Actor",
+      "age": 54,
+      "birthplace": "Sydney, Australia"
+    },
+    {
+      "name": "Gwyneth Paltrow",
+      "occupation": "Actor",
+      "age": 50,
+      "birthplace": "Los Angeles, California"
+    },
+    {
+      "name": "Jennifer Lopez",
+      "occupation": "Singer",
+      "age": 54,
+      "birthplace": "The Bronx, New York"
+    },
+    {
+      "name": "Chris Evans",
+      "occupation": "Actor",
+      "age": 42,
+      "birthplace": "Boston, Massachusetts"
+    }
+  ]
+  function mutassVéletlenszerűHírességet() {
+    var véletlenIndex = Math.floor(Math.random() * hírességek.length);
+    var véletlenHíresség = hírességek[véletlenIndex];
+
+    var hírességElem = document.getElementById("híresség");
+    hírességElem.textContent = "name: " + véletlenHíresség.name + ", occupation: " + véletlenHíresség.occupation + ", age: " + véletlenHíresség.age + ", birthplace: " + véletlenHíresség.birthplace;
+}
+  
